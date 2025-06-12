85c68dc7b8746efce0907f17f163e7d1b2aaed6d
Itsnope <noverjkd@gmail.com>
2025-06-12 22:27:41 +0700

    [ADD] sesi1-db-index.php e-meeting-app

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>E-Meeting</title>
+</head>
+<body>
+  <h1>Selamat Datang di Aplikasi E-Meeting</h1>
+  <p>Ini adalah platform untuk mengatur jadwal meeting dengan Google Calendar API.</p>
+  <p>Waktu Server: <?= date("d F Y, H:i:s") ?></p>
+</body>
+</html>
